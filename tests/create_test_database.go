50fc1513f5077d69af40fb5535d0985c9425d8ed
Fascinat0r <fascinator2904@gmail.com>
2024-07-27 20:22:54 +0300

    Add creation test database

---
+++ tests/create_test_database.go
@@ -0,0 +1,50 @@
+package tests
+
+import (
+	"ChaikaGoods/internal/config"
+	"context"
+	"fmt"
+	"os/exec"
+)
+
+func createDatabase(ctx context.Context, storageCfg config.StorageConfig) error {
+	cmdStr := fmt.Sprintf("createdb -h %s -p %s -U %s %s",
+		storageCfg.Host, storageCfg.Port, storageCfg.User, storageCfg.Database)
+
+	cmd := exec.CommandContext(ctx, "bash", "-c", cmdStr)
+	cmd.Env = append(cmd.Env, fmt.Sprintf("PGPASSWORD=%s", storageCfg.Password))
+
+	if output, err := cmd.CombinedOutput(); err != nil {
+		return fmt.Errorf("failed to create database: %v, output: %s", err, string(output))
+	}
+
+	return nil
+}
+
+func restoreDatabase(ctx context.Context, storageCfg config.StorageConfig, dumpFile string) error {
+	cmdStr := fmt.Sprintf("psql -h %s -p %s -U %s -d %s -f %s",
+		storageCfg.Host, storageCfg.Port, storageCfg.User, storageCfg.Database, dumpFile)
+
+	cmd := exec.CommandContext(ctx, "bash", "-c", cmdStr)
+	cmd.Env = append(cmd.Env, fmt.Sprintf("PGPASSWORD=%s", storageCfg.Password))
+
+	if output, err := cmd.CombinedOutput(); err != nil {
+		return fmt.Errorf("failed to restore database: %v, output: %s", err, string(output))
+	}
+
+	return nil
+}
+
+func deleteDatabase(ctx context.Context, storageCfg config.StorageConfig) error {
+	cmdStr := fmt.Sprintf("dropdb -h %s -p %s -U %s %s",
+		storageCfg.Host, storageCfg.Port, storageCfg.User, storageCfg.Database)
+
+	cmd := exec.CommandContext(ctx, "bash", "-c", cmdStr)
+	cmd.Env = append(cmd.Env, fmt.Sprintf("PGPASSWORD=%s", storageCfg.Password))
+
+	if output, err := cmd.CombinedOutput(); err != nil {
+		return fmt.Errorf("failed to delete database: %v, output: %s", err, string(output))
+	}
+
+	return nil
+}
