56d7ea34bb00823a14e1b3179159a99fff556ef4
Fascinat0r <fascinator2904@gmail.com>
2024-07-28 18:40:57 +0300

    Add first endpoints, draft

--- internal/handler/endpoint.go
+++ internal/handler/endpoint.go
@@ -1,9 +1,13 @@
 package handler
 
 import (
+	"ChaikaGoods/internal/handler/schemas"
 	"ChaikaGoods/internal/service"
+	"context"
+	"errors"
 	"github.com/go-kit/kit/endpoint"
 	"github.com/go-kit/log"
+	"github.com/go-kit/log/level"
 )
 
 // Endpoints holds all Go kit endpoints for all operations
@@ -35,5 +39,71 @@ func MakeEndpoints(logger log.Logger, service service.Service) Endpoints {
 		AddProduct:    makeAddProductEndpoint(logger, service),
 		UpdateProduct: makeUpdateProductEndpoint(logger, service),
 		DeleteProduct: makeDeleteProductEndpoint(logger, service),
+	}
+}
+
+// makeGetAllProductsEndpoint constructs a GetAllProducts endpoint wrapping the service.
+func makeGetAllProductsEndpoint(logger log.Logger, s service.Service) endpoint.Endpoint {
+	return func(ctx context.Context, request interface{}) (interface{}, error) {
+		_, ok := request.(schemas.GetAllProductsRequest)
+		if !ok {
+			_ = level.Error(logger).Log("msg", "invalid request type")
+			return nil, errors.New("invalid request type")
+		}
+		products, err := s.GetAllProducts(ctx)
+		return schemas.GetAllProductsResponse{Products: products}, err
+
+	}
+}
+
+// makeGetCurrentVersionEndpoint constructs a GetCurrentVersion endpoint wrapping the service.
+func makeGetCurrentVersionEndpoint(logger log.Logger, s service.Service) endpoint.Endpoint {
+	return func(ctx context.Context, request interface{}) (interface{}, error) {
+		_, ok := request.(schemas.GetCurrentVersionRequest)
+		if !ok {
+			_ = level.Error(logger).Log("msg", "invalid request type")
+			return nil, errors.New("invalid request type")
+		}
+		version, err := s.GetCurrentVersion(ctx)
+		return schemas.GetCurrentVersionResponse{Version: version}, err
+	}
+}
+
+// makeGetDeltaEndpoint constructs a GetDelta endpoint wrapping the service.
+func makeGetDeltaEndpoint(logger log.Logger, s service.Service) endpoint.Endpoint {
+	return func(ctx context.Context, request interface{}) (interface{}, error) {
+		req, ok := request.(schemas.GetDeltaRequest)
+		if !ok {
+			_ = level.Error(logger).Log("msg", "invalid request type")
+			return nil, errors.New("invalid request type")
+		}
+		changes, err := s.GetDelta(ctx, req.VersionID)
+		return schemas.GetDeltaResponse{Changes: changes}, err
+	}
+}
+
+// makeSearchPacketEndpoint constructs a SearchPacket endpoint wrapping the service.
+func makeSearchPacketEndpoint(logger log.Logger, s service.Service) endpoint.Endpoint {
+	return func(ctx context.Context, request interface{}) (interface{}, error) {
+		req, ok := request.(schemas.SearchPacketRequest)
+		if !ok {
+			_ = level.Error(logger).Log("msg", "invalid request type")
+			return nil, errors.New("invalid request type")
+		}
+		packets, err := s.SearchPacket(ctx, req.Query, req.Limit, req.Offset)
+		return schemas.SearchPacketResponse{Packets: packets}, err
+	}
+}
+
+// makeAddPacketEndpoint constructs a AddPacket endpoint wrapping the service.
+func makeAddPacketEndpoint(logger log.Logger, s service.Service) endpoint.Endpoint {
+	return func(ctx context.Context, request interface{}) (interface{}, error) {
+		req, ok := request.(schemas.AddPacketRequest)
+		if !ok {
+			_ = level.Error(logger).Log("msg", "invalid request type")
+			return nil, errors.New("invalid request type")
+		}
+		id, err := s.AddPacket(ctx, &req.Packet, req.PacketContent)
+		return schemas.AddPacketResponse{PacketID: id}, err
 	}
 }
