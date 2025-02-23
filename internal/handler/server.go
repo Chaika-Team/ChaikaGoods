4186caf4d0a2ea72814a886d899a87ecdad2ccf4
doutyn <dmitryfolomkin@rambler.ru>
2025-02-23 15:48:18 +0300

    [TESTS] Server unit tests

--- internal/handler/server.go
+++ internal/handler/server.go
@@ -33,7 +33,7 @@ func NewHTTPServer(logger log.Logger, endpoints Endpoints) http.Handler {
 	r.PathPrefix("/docs/").Handler(httpSwagger.WrapHandler)
 
 	// Register file and folder routes
-	registerRoutes(logger, r.PathPrefix("/api/v1").Subrouter(), endpoints)
+	RegisterRoutes(logger, r.PathPrefix("/api/v1").Subrouter(), endpoints)
 
 	return r
 }
@@ -61,62 +61,62 @@ func registerRoutes(logger log.Logger, api *mux.Router, endpoints Endpoints) {
 	// Get product by ID
 	api.Methods("GET").Path("/products/{id}").Handler(httpGoKit.NewServer(
 		endpoints.GetProductByID,
-		decodeRequestWithID(logger, "id", &schemas.GetProductByIDRequest{}),
-		encodeResponse(logger),
-		httpGoKit.ServerErrorEncoder(encodeErrorResponse(logger)),
+		DecodeRequestWithID(logger, "id", &schemas.GetProductByIDRequest{}),
+		EncodeResponse(logger),
+		httpGoKit.ServerErrorEncoder(EncodeErrorResponse(logger)),
 	))
 
 	// Search Template
 	api.Methods("GET").Path("/templates/search").Handler(httpGoKit.NewServer(
 		endpoints.SearchTemplates,
-		decodeSearchTemplatesRequest,
-		encodeResponse(logger),
-		httpGoKit.ServerErrorEncoder(encodeErrorResponse(logger)),
+		DecodeSearchTemplatesRequest,
+		EncodeResponse(logger),
+		httpGoKit.ServerErrorEncoder(EncodeErrorResponse(logger)),
 	))
 
 	// Add Template
 	api.Methods("POST").Path("/templates").Handler(httpGoKit.NewServer(
 		endpoints.AddTemplate,
-		decodeJSONRequest(&schemas.AddTemplateRequest{}),
-		encodeResponse(logger),
-		httpGoKit.ServerErrorEncoder(encodeErrorResponse(logger)),
+		DecodeJSONRequest(&schemas.AddTemplateRequest{}),
+		EncodeResponse(logger),
+		httpGoKit.ServerErrorEncoder(EncodeErrorResponse(logger)),
 	))
 
 	// Get Template by ID
 	api.Methods("GET").Path("/templates/{id}").Handler(httpGoKit.NewServer(
 		endpoints.GetTemplateByID,
-		decodeRequestWithID(logger, "id", &schemas.GetTemplateByIDRequest{}),
-		encodeResponse(logger),
-		httpGoKit.ServerErrorEncoder(encodeErrorResponse(logger)),
+		DecodeRequestWithID(logger, "id", &schemas.GetTemplateByIDRequest{}),
+		EncodeResponse(logger),
+		httpGoKit.ServerErrorEncoder(EncodeErrorResponse(logger)),
 	))
 
 	// Add product
 	api.Methods("POST").Path("/products").Handler(httpGoKit.NewServer(
 		endpoints.CreateProduct,
-		decodeJSONRequest(&schemas.CreateProductRequest{}),
-		encodeResponse(logger),
-		httpGoKit.ServerErrorEncoder(encodeErrorResponse(logger)),
+		DecodeJSONRequest(&schemas.CreateProductRequest{}),
+		EncodeResponse(logger),
+		httpGoKit.ServerErrorEncoder(EncodeErrorResponse(logger)),
 	))
 
 	// Update product
 	api.Methods("PUT").Path("/products/{id}").Handler(httpGoKit.NewServer(
 		endpoints.UpdateProduct,
-		decodeJSONRequest(&schemas.UpdateProductRequest{}),
-		encodeResponse(logger),
-		httpGoKit.ServerErrorEncoder(encodeErrorResponse(logger)),
+		DecodeJSONRequest(&schemas.UpdateProductRequest{}),
+		EncodeResponse(logger),
+		httpGoKit.ServerErrorEncoder(EncodeErrorResponse(logger)),
 	))
 
 	// Delete product
 	api.Methods("DELETE").Path("/products/{id}").Handler(httpGoKit.NewServer(
 		endpoints.DeleteProduct,
-		decodeRequestWithID(logger, "id", &schemas.DeleteProductRequest{}),
-		encodeResponse(logger),
-		httpGoKit.ServerErrorEncoder(encodeErrorResponse(logger)),
+		DecodeRequestWithID(logger, "id", &schemas.DeleteProductRequest{}),
+		EncodeResponse(logger),
+		httpGoKit.ServerErrorEncoder(EncodeErrorResponse(logger)),
 	))
 }
 
 // encodeResponse encodes the response as JSON.
-func encodeResponse(_ log.Logger) httpGoKit.EncodeResponseFunc {
+func EncodeResponse(_ log.Logger) httpGoKit.EncodeResponseFunc {
 	return func(ctx context.Context, w http.ResponseWriter, response interface{}) error {
 		// Common response encoding logic here
 		return json.NewEncoder(w).Encode(response)
@@ -124,7 +124,7 @@ func encodeResponse(_ log.Logger) httpGoKit.EncodeResponseFunc {
 }
 
 // encodeErrorResponse encodes the error response as JSON with appropriate status code.
-func encodeErrorResponse(logger log.Logger) httpGoKit.ErrorEncoder {
+func EncodeErrorResponse(logger log.Logger) httpGoKit.ErrorEncoder {
 	return func(ctx context.Context, err error, w http.ResponseWriter) {
 		var status int
 		var response interface{}
@@ -154,19 +154,13 @@ func encodeErrorResponse(logger log.Logger) httpGoKit.ErrorEncoder {
 	}
 }
 
-func decodeJSONRequest(schema interface{}) httpGoKit.DecodeRequestFunc {
+func DecodeJSONRequest(schema interface{}) httpGoKit.DecodeRequestFunc {
 	return func(ctx context.Context, req *http.Request) (interface{}, error) {
-		if req.Body == nil {
+		defer req.Body.Close()
+		err := json.NewDecoder(req.Body).Decode(schema)
+		if err == io.EOF {
 			return nil, errors.New("empty request body")
-		}
-		defer func(Body io.ReadCloser) {
-			err := Body.Close()
-			if err != nil {
-				_ = level.Error(log.NewNopLogger()).Log("msg", "error closing request body", "err", err)
-			}
-		}(req.Body)
-
-		if err := json.NewDecoder(req.Body).Decode(schema); err != nil {
+		} else if err != nil {
 			return nil, err
 		}
 		return schema, nil
@@ -174,7 +168,7 @@ func decodeJSONRequest(schema interface{}) httpGoKit.DecodeRequestFunc {
 }
 
 // decodeRequestWithID генерирует DecodeRequestFunc для запросов с ID в пути.
-func decodeRequestWithID(logger log.Logger, paramName string, schema interface{}) httpGoKit.DecodeRequestFunc {
+func DecodeRequestWithID(logger log.Logger, paramName string, schema interface{}) httpGoKit.DecodeRequestFunc {
 	return func(ctx context.Context, req *http.Request) (interface{}, error) {
 		id, err := extractID(req, paramName)
 		if err != nil {
@@ -208,7 +202,7 @@ func decodeEmptyRequest[T any]() func(context.Context, *http.Request) (interface
 }
 
 // decodeSearchTemplatesRequest декодирует GET запрос с параметрами query, limit и offset.
-func decodeSearchTemplatesRequest(_ context.Context, req *http.Request) (interface{}, error) {
+func DecodeSearchTemplatesRequest(_ context.Context, req *http.Request) (interface{}, error) {
 	query := req.URL.Query()
 
 	searchString := query.Get("query")
